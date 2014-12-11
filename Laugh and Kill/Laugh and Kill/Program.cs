21e497d0c37f22b4e7c0e63adca63af5b163b2ec
RapidWarp <barna.benke@gmail.com>
2014-12-11 10:52:29 +0100

    new file:   Laugh and Kill/Laugh and Kill.sln 	new file:   Laugh and Kill/Laugh and Kill.v12.suo 	new file:   Laugh and Kill/Laugh and Kill/App.config 	new file:   Laugh and Kill/Laugh and Kill/Laugh and Kill.csproj 	new file:   Laugh and Kill/Laugh and Kill/Program.cs 	new file:   Laugh and Kill/Laugh and Kill/Properties/AssemblyInfo.cs 	new file:   Laugh and Kill/Laugh and Kill/bin/Debug/Laugh and Kill.vshost.exe 	new file:   Laugh and Kill/Laugh and Kill/bin/Debug/Laugh and Kill.vshost.exe.config 	new file:   Laugh and Kill/Laugh and Kill/bin/Debug/LeagueSharp.Common.dll 	new file:   Laugh and Kill/Laugh and Kill/bin/Debug/LeagueSharp.dll 	new file:   Laugh and Kill/Laugh and Kill/bin/Debug/LeagueSharp.xml 	new file:   Laugh and Kill/Laugh and Kill/bin/Debug/SharpDX.Direct3D9.dll 	new file:   Laugh and Kill/Laugh and Kill/bin/Debug/SharpDX.dll 	new file:   Laugh and Kill/Laugh and Kill/bin/Release/Laugh and Kill.exe.config 	new file:   Laugh and Kill/Laugh and Kill/bin/Release/Laugh and Kill.pdb 	new file:   Laugh and Kill/Laugh and Kill/bin/Release/LeagueSharp.Common.dll 	new file:   Laugh and Kill/Laugh and Kill/bin/Release/LeagueSharp.dll 	new file:   Laugh and Kill/Laugh and Kill/bin/Release/LeagueSharp.xml 	new file:   Laugh and Kill/Laugh and Kill/bin/Release/SharpDX.Direct3D9.dll 	new file:   Laugh and Kill/Laugh and Kill/bin/Release/SharpDX.dll

---
+++ Laugh and Kill/Laugh and Kill/Program.cs
@@ -0,0 +1,68 @@
+﻿
+#region
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using SharpDX;
+using LeagueSharp;
+using LeagueSharp.Common;
+#endregion
+
+namespace Laugh_and_Kill
+{
+    internal class Program
+    {
+
+        private static Menu Config;
+
+        private static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += Game_OnGameLoad;
+        }
+
+        private static void Game_OnGameLoad(EventArgs args)
+        {
+            Config = new Menu("Laugh", "Laugh", true);
+            Config.AddSubMenu(new Menu("On/Off", "On/Off"));
+            Config.SubMenu("On/Off")
+                .AddItem(new MenuItem("EmoteToggable", "Toggleable Emote"))
+                .SetValue(new KeyBind('h', KeyBindType.Toggle));
+            Config.SubMenu("On/Off").AddItem(new MenuItem("Type", "Spam Laughing??")).SetValue(true);
+
+            Config.AddToMainMenu();
+
+            Game.OnGameNotifyEvent += Game_OnGameNotifyEvent;
+
+            Game.PrintChat("Laugh on Kill loaded by RapidWarp, credits to The Kush Style for emote spammer");
+        }
+
+        private static void SPAM()
+        {
+            if ((Config.Item("Type").GetValue<Slider>().Value) != 1) return;
+            Packet.C2S.Emote.Encoded(new Packet.C2S.Emote.Struct(2)).Send();
+            Packet.C2S.Move.Encoded(new Packet.C2S.Move.Struct(Game.CursorPos.X, Game.CursorPos.Y)).Send();
+        }
+
+        private static void Game_OnGameNotifyEvent(GameNotifyEventArgs args)
+        {
+            if (args.NetworkId != ObjectManager.Player.NetworkId)
+            {
+                return;
+            }
+
+            switch (args.EventId)
+            {
+                case GameEventId.OnChampionDoubleKill:
+                {
+                    if (Config.Item("EmoteToggable").GetValue<KeyBind>().Active)
+                    {
+                        SPAM();
+                    }
+                }
+                    break;
+            }
+        }
+    }
+}
